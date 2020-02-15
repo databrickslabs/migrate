56830eba2ba57976e0330462b7faa67b85fa9291
Douglas Moore <dmoore247@gmail.com>
2020-02-14 21:58:18 -0500

    Initial commit for package setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="databricks-migration", # Replace with your own username
+    version="0.0.1",
+    author="Miklos C",
+    author_email="miklos.christine@databricks.com",
+    description="Databricks Migration scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrchristine/db-migration",
+    packages=setuptools.find_packages(),
+    py_modules=["export_db","import_db","test_connection"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
