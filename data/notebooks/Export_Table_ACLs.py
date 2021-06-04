915ab9108dfd50d31910eb53e4f999392db5a1f7
tomidatabricks <tomi.schumacher@datarbricks.com>
2021-06-04 14:48:24 -0700

    added exception handling in notebooks so that upon a SQL exception export/import keeps going and export tracks errors as special rows in the output file added output values to the import and export notebook and added code to display those in export_db and import_db added retry logic to notebook job run polling, as on large workspaces this is a multi hour process and fails at times - retries for just notebook polls fixed that extended documentation in notebooks (2 first cells) - this documentation will be the bases for the readme file (minus some details, plus some tool param description) I still did not have a completely succuess full run for an export acls in demo.cloud.databricks.com but I am very close (it was just a bug in the retrieval of the result from the notebook)
    Checked this in as it close to done - so it can be reviewed

--- data/notebooks/Export_Table_ACLs.py
+++ data/notebooks/Export_Table_ACLs.py
@@ -7,17 +7,52 @@
 # MAGIC - Databases: [Optional] comma separated list of databases to be exported, if empty all databases will be exported
 # MAGIC - OutputPath: Path to write the exported file to 
 # MAGIC 
+# MAGIC Returns: (`dbutils.notebook.exit(exit_JSON_string)`)
+# MAGIC - `{ "total_num_acls": <int>, "num_errors": <int> }` 
+# MAGIC   - total_num_acls : valid ACL entries int the exported JSON
+# MAGIC   - num_errors : error entries in the exported JSON, principal is set to `ERROR_!!!` and object_key and object_value are prefixed with `ERROR_!!!`
+# MAGIC 
 # MAGIC Execution: **Run the notebook on a cluster with Table ACL's enabled as a user who is an admin** 
 # MAGIC 
-# MAGIC Supportes ACLs for Object types:
+# MAGIC Supported object types:
 # MAGIC - Catalog: included if all databases are exported, not included if databases to be exported are specified
 # MAGIC   - Database: included
 # MAGIC     - Table: included
-# MAGIC     - View: included
+# MAGIC     - View: included (they are treated as tables with ObjectType `TABLE`)
 # MAGIC - Anonymous Function: included (testing pending)
 # MAGIC - Any File: included
 # MAGIC 
-# MAGIC Disclaimer: This notebook is still needs some more testing, check back soon as fixes might have been added.
+# MAGIC Unsupported object types:
+# MAGIC - User Function: Currently in Databricks SQL not supported - will add support later
+# MAGIC 
+# MAGIC JSON File format: Line of JSON objects, gzipped
+# MAGIC 
+# MAGIC - written as `.coalesce(1).format("JSON").option("compression","gzip")`
+# MAGIC - each line contains a JSON object with the keys:
+# MAGIC   - `Database`: string
+# MAGIC   - `Principal`: string
+# MAGIC   - `ActionTypes`: list of action strings: 
+# MAGIC   - `ObjectType`: `(ANONYMOUS_FUNCTION|ANY_FILE|CATALOG$|DATABASE|TABLE|ERROR_!!!_<type>)` (view are treated as tables)
+# MAGIC   - `ObjectKey`: string
+# MAGIC   - `ExportTimestamp`: string
+# MAGIC - error lines contains:
+# MAGIC   - the special `Principal` `ERROR_!!!` 
+# MAGIC   - `ActionTypes` contains one element: the error message, starting with `ERROR!!! :`
+# MAGIC   - `Database`, `ObjectType`, `ObjectKey` are all prefixed with `ERROR_!!!_`
+# MAGIC - error lines are ignored by the Import_Table_ACLs 
+# MAGIC 
+# MAGIC The JSON file is written as table, because on a cluster with Table ACLS activated, files cannot be written directly.
+# MAGIC The output path will contain other files and diretories, starting with `_`, which can be ignored.
+# MAGIC 
+# MAGIC 
+# MAGIC What to do if exported JSON contains errors (the notebook returns `num_errors` > 0):
+# MAGIC - If there are only a few errors ( e.g. less then 1% or less then dozen)
+# MAGIC   - proceed with the import (any error lines will be ignored)
+# MAGIC   - For each error, the object type, name and error message is included so the cause can be investigated
+# MAGIC     - in most cases, it turns out that those are broken or not used tables or views
+# MAGIC - If there are many errors
+# MAGIC   - Try executing some `SHOW GRANT` commands on the same cluster using the same user, there might be a underlying problem
+# MAGIC   - review the errors and investiage
 
 # COMMAND ----------
 
@@ -39,7 +74,35 @@ if not dbutils.widgets.get("OutputPath").startswith("dbfs:/"):
 # DBTITLE 1,Define Export Logic
 import pyspark.sql.functions as sf
 from typing import Callable, Iterator, Union, Optional, List
-import datetime;
+import datetime
+import sys
+
+def create_error_grants_df(sys_exec_info_res, database_name: str,object_type: str, object_key: str):
+  msg_context = f"context: database_name: {database_name}, object_type: {object_type}, object_key: {object_key}"
+
+  msg_lines = str(sys_exec_info_res[1]).split("\n")
+  if len(msg_lines) <= 2:
+    short_message = " ".join(msg_lines)
+  else:
+    short_message = " ".join(msg_lines[:2])   
+  error_message = f"ERROR!!! : exception class {sys_exec_info_res[0]},  message: {short_message}, {msg_context}".replace('"',"'")
+
+  print(error_message)
+
+  database_value = f"'ERROR_!!!_{database_name}'".replace('"',"'") if database_name else "NULL"
+  object_key_value = f"'ERROR_!!!_{object_key}'".replace('"',"'") if object_key else "NULL"
+  object_type_value = f"'ERROR_!!!_{object_type}'".replace('"',"'") if object_type else "NULL" # Import ignores this object type
+
+  grants_df = spark.sql(f"""SELECT 
+      {database_value} AS Database, 
+      'ERROR_!!!' AS Principal,
+      array("{error_message}") AS ActionTypes, 
+      {object_type_value} AS ObjectType, 
+      {object_key_value} AS ObjectKey, 
+      Now() AS ExportTimestamp
+   """)  
+
+  return grants_df
 
 def get_database_names():
   database_names = []
@@ -50,20 +113,24 @@ def get_database_names():
       database_names.append(db.namespace)
   return database_names
 
-def create_grants_df(database_name: str,object_type: str, object_key: str) -> List[str]:
-  if object_type in ["CATALOG", "ANY FILE", "ANONYMOUS FUNCTION"]: #without object key
-     grants_df = (
-       spark.sql(f"SHOW GRANT ON {object_type}")
-       .groupBy("ObjectType","ObjectKey","Principal").agg(sf.collect_set("ActionType").alias("ActionTypes"))
-       .selectExpr("NULL AS Database","Principal","ActionTypes","ObjectType","ObjectKey","Now() AS ExportTimestamp")
-     )
-  else: 
-    grants_df = (
-      spark.sql(f"SHOW GRANT ON {object_type} {object_key}")
-      .filter(sf.col("ObjectType") == f"{object_type}")
-      .groupBy("ObjectType","ObjectKey","Principal").agg(sf.collect_set("ActionType").alias("ActionTypes"))
-      .selectExpr(f"'{database_name}' AS Database","Principal","ActionTypes","ObjectType","ObjectKey","Now() AS ExportTimestamp")
-    )  
+def create_grants_df(database_name: str,object_type: str, object_key: str):
+  try:
+    if object_type in ["CATALOG", "ANY FILE", "ANONYMOUS FUNCTION"]: #without object key
+       grants_df = (
+         spark.sql(f"SHOW GRANT ON {object_type}")
+         .groupBy("ObjectType","ObjectKey","Principal").agg(sf.collect_set("ActionType").alias("ActionTypes"))
+         .selectExpr("NULL AS Database","Principal","ActionTypes","ObjectType","ObjectKey","Now() AS ExportTimestamp")
+       )
+    else: 
+      grants_df = (
+        spark.sql(f"SHOW GRANT ON {object_type} {object_key}")
+        .filter(sf.col("ObjectType") == f"{object_type}")
+        .groupBy("ObjectType","ObjectKey","Principal").agg(sf.collect_set("ActionType").alias("ActionTypes"))
+        .selectExpr(f"'{database_name}' AS Database","Principal","ActionTypes","ObjectType","ObjectKey","Now() AS ExportTimestamp")
+      )  
+  except:  
+    grants_df = create_error_grants_df(sys.exc_info(), database_name, object_type, object_key)
+    
   return grants_df
   
 
@@ -77,6 +144,9 @@ def create_table_ACLSs_df_for_databases(database_names: List[str]):
     include_catalog = True
   else:
     include_catalog = False
+    
+  num_databases_processed = len(database_names)
+  num_tables_or_views_processed = 0
 
   # ANONYMOUS FUNCTION
   combined_grant_dfs = create_grants_df(None, "ANONYMOUS FUNCTION", None)
@@ -100,25 +170,32 @@ def create_table_ACLSs_df_for_databases(database_names: List[str]):
       create_grants_df(database_name, "DATABASE", database_name)
     )
     
-    tables_and_views_rows = spark.sql(
-      f"SHOW TABLES IN {database_name}"
-    ).filter(sf.col("isTemporary") == False).collect()
-    
-    print(f"{datetime.datetime.now()} working on database {database_name} with {len(tables_and_views_rows)} tables and views")
-    for table_row in tables_and_views_rows:
-      
-      # TABLE, VIEW
+    try:
+      tables_and_views_rows = spark.sql(
+        f"SHOW TABLES IN {database_name}"
+      ).filter(sf.col("isTemporary") == False).collect()
+
+      print(f"{datetime.datetime.now()} working on database {database_name} with {len(tables_and_views_rows)} tables and views")
+      num_tables_or_views_processed = num_tables_or_views_processed + len(tables_and_views_rows)
+     
+      for table_row in tables_and_views_rows:
+
+        # TABLE, VIEW
+        combined_grant_dfs = combined_grant_dfs.unionAll(
+          create_grants_df(database_name, "TABLE", f"{table_row.database}.{table_row.tableName}")
+        )
+    except:  
+      # error in SHOW TABLES IN database_name, errors in create_grants_df have already been catched
       combined_grant_dfs = combined_grant_dfs.unionAll(
-        create_grants_df(database_name, "TABLE", f"{table_row.database}.{table_row.tableName}")
+        create_error_grants_df(sys.exc_info(), database_name ,"DATABASE", database_name)
       )
-   
+      
+     
     #TODO ADD USER FUNCTION - not supported in SQL Analytics, so this can wait a bit
     # ... SHOW USER FUNCTIONS LIKE  <my db>.`*`;
     #. function_row['function']   ... nah does not seem to work
-    
-  #combined_grant_dfs = combined_grant_dfs.sort("")
-      
-  return combined_grant_dfs
+          
+  return combined_grant_dfs, num_databases_processed, num_tables_or_views_processed
 
 
 # COMMAND ----------
@@ -135,8 +212,9 @@ else:
   print(f"Exporting the following databases: {databases}")
 
 
-table_ACLs_df = create_table_ACLSs_df_for_databases(databases)
+table_ACLs_df,num_databases_processed, num_tables_or_views_processed = create_table_ACLSs_df_for_databases(databases)
 
+print(f"{datetime.datetime.now()} total number processed: databases: {num_databases_processed}, tables or views: {num_tables_or_views_processed}")
 print(f"{datetime.datetime.now()} writing table ACLs to {output_path}")
 
 # with table ACLS active, I direct write to DBFS is not allowed, so we store
@@ -156,11 +234,35 @@ print(f"{datetime.datetime.now()} writing table ACLs to {output_path}")
 
 # COMMAND ----------
 
+# DBTITLE 1,Exported Table ACLs
 display(spark.read.format("json").load(output_path)) 
 
 # COMMAND ----------
 
 print(output_path)
+
+# COMMAND ----------
+
+# DBTITLE 1,Write total_num_acls and num_errors to the notebook exit value
+totals_df = (spark.read
+      .format("JSON")
+      .load(output_path)
+      .selectExpr(
+         "sum(1) AS total_num_acls"
+        ,"sum(CASE WHEN Principal = 'ERROR_!!!' THEN 1 ELSE 0 END) AS num_errors")
+     )
+
+res_rows = totals_df.collect()
+
+exit_JSON_string = '{ "total_num_acls": '+str(res_rows[0]["total_num_acls"])+', "num_errors": '+str(res_rows[0]["num_errors"])+' }'
+
+print(exit_JSON_string)
+
+dbutils.notebook.exit(exit_JSON_string) 
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
