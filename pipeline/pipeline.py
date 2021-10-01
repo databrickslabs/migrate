c64a9f3018953861640b7be7e01a3d1f712fe4ae
Yubing Yin <yubing.yin@databricks.com>
2021-10-01 15:00:53 -0700

    Pipeline interface draft

---
+++ pipeline/pipeline.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from task import AbstractTask
+
+
+@dataclass
+class PipelineConfig:
+    """Configuration of the workspace migration pipeline."""
+    base_dir: str
+    session: str
+
+
+class Pipeline:
+    """Class that coordinates run of a group of tasks, which form a DAG based on dependencies
+    defined in add_task().
+
+    The pipeline has built-in checkpoint i.e it will skip all complete tasks upon restart."""
+
+    @dataclass
+    class Node:
+        task: AbstractTask = None
+        children = []
+
+    def __init__(self, checkpoint: Optional[str]):
+        self._source = self.Node()
+        self._checkpoint = checkpoint
+
+    def add_task(self, task: AbstractTask, parents: Optional[List[Node]] = None):
+        node = self.Node(task)
+        if not parents:
+            parents = [self._source]
+        for parent in parents:
+            parent.children.append(node)
+
+    def run(self):
+        pass
+
+
+def build_pipeline(config: PipelineConfig) -> Pipeline:
+    """Build the pipeline based on the config."""
+    checkpoint = None
+    pipeline = Pipeline(checkpoint)
+
+    # Example:
+    #
+    # task1 = ...
+    # task2 = ...
+    # task3 = ...
+    # task4 = ...
+    # node1 = pipeline.add_task(task1)
+    # node2 = pipeline.add_task(task2, [node1])
+    # node3 = pipeline.add_task(task3, [node1])
+    # node4 = pipeline.add_task(task4, [node2, node3])
+    #
+    # Execution order: task1 -> task2 & task3 -> task4.
+    #
+    return pipeline
