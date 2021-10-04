f3bf8b619a2286784cb26b4e450a815eb6ed28fc
Yubing Yin <yubing.yin@databricks.com>
2021-10-04 10:06:43 -0700

    Add comments per Kevin's comments

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -19,19 +19,28 @@ class Pipeline:
 
     @dataclass
     class Node:
+        """Node within a pipeline.
+
+        task: The task runs in the node.
+        children: The nodes that should run after the current node completes.
+        """
         task: AbstractTask = None
         children = []
 
-    def __init__(self, checkpoint: Optional[str]):
+    def __init__(self, working_dir: Optional[str]):
+        """
+        :param working_dir: the dir where the pipeline reads / writes checkpoints and outputs logs.
+        """
         self._source = self.Node()
-        self._checkpoint = checkpoint
+        self._working_dir = working_dir
 
-    def add_task(self, task: AbstractTask, parents: Optional[List[Node]] = None):
+    def add_task(self, task: AbstractTask, parents: Optional[List[Node]] = None) -> Node:
         node = self.Node(task)
         if not parents:
             parents = [self._source]
         for parent in parents:
             parent.children.append(node)
+        return node
 
     def run(self):
         pass
