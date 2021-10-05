3bff54b9af5d361daaf4d7b0a078a92f01b414f3
Sourav Khandelwal <sourav.khandelwal@databricks.com>
2021-10-04 18:18:57 -0700

    Made fixes to users import script and notebook script

---
+++ dbclient/test/ScimClientTest.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import MagicMock
+from dbclient import ScimClient
+from dbclient.test.TestUtils import TEST_CONFIG
+
+class TestScimClient(unittest.TestCase):
+
+    def test_get_old_user_emails(self):
+        scimClient =  ScimClient(TEST_CONFIG)
+        scimClient.get_export_dir = MagicMock(return_value ="")
+        user_list = scimClient.get_users_from_log("test_users.log")
+        self.assertEqual(len(user_list), 2)
+
+    def test_get_old_user_emails(self):
+        scimClient =  ScimClient(TEST_CONFIG)
+        scimClient.get_export_dir = MagicMock(return_value ="")
+        old_user_map = scimClient.get_old_user_emails("test_users.log")
+        self.assertEqual(old_user_map['29'], 'sourav.khandelwal@databricks.com')
+        self.assertEqual(old_user_map['20'], 'test@databricks.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ dbclient/test/TestUtils.py
@@ -0,0 +1,11 @@
+TEST_CONFIG = {
+    'token': 'test_token',
+    'url': 'test_url',
+    'export_dir': '/',
+    'is_aws': 'True',
+    'skip_failed': 'True',
+    'verbose': 'False',
+    'verify_ssl': 'False',
+    'file_format': 'DBC',
+    'overwrite_notebooks': 'True'
+}
