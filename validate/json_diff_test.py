b0eae72e3823104876383809e1feeb37f6ddec9d
Yubing Yin <yubing.yin@databricks.com>
2021-12-01 16:01:17 -0800

    Implement json_diff library for data integrity verification.

---
+++ validate/json_diff_test.py
@@ -0,0 +1,36 @@
+import unittest
+from json_diff import *
+
+
+class JsonDiffTest(unittest.TestCase):
+    def test_equal(self):
+        self.assertEqual(None, diff_json(1, 1))
+        self.assertEqual(None, diff_json(2.0, 2.0))
+        self.assertEqual(None, diff_json('hello world', 'hello world'))
+        self.assertEqual(None, diff_json({'i': 1, 'f': 2.0, 's': 'hello world'},
+                                         {'i': 1, 'f': 2.0, 's': 'hello world'}))
+
+    def test_value_diff(self):
+        self.assertEqual(ValueDiff(1, 2), diff_json(1, 2))
+        self.assertEqual(ValueDiff(1.0, 2.0), diff_json(1.0, 2.0))
+        self.assertEqual(ValueDiff('hello', 'world'), diff_json('hello', 'world'))
+
+    def test_type_diff(self):
+        self.assertEqual(TypeDiff(1, 2.0), diff_json(1, 2.0))
+        self.assertEqual(TypeDiff('hello', 3), diff_json('hello', 3))
+        self.assertEqual(TypeDiff(2.0, {}), diff_json(2.0, {}))
+        self.assertEqual(TypeDiff({}, 1), diff_json({}, 1))
+
+    def test_dict_diff(self):
+        expected = DictDiff()
+        expected.add_child('i', ValueDiff(1, 2))
+        expected.add_child('f', TypeDiff(2.0, 3))
+        expected.add_child('s', ValueDiff('hello', 'world'))
+        expected.add_child('l', Miss('RIGHT', 'left'))
+        expected.add_child('r', Miss('LEFT', 'right'))
+        self.assertEqual(expected, diff_json({'i': 1, 'f': 2.0, 's': 'hello', 'l': 'left'},
+                                             {'f': 3, 's': 'world', 'i': 2, 'r': 'right'}))
+
+
+if __name__ == '__main__':
+    unittest.main()
