be1a9de86ef6e43d4929b267feab6dda5973c19e
Kevin Kim <kevinkim@databricks.com>
2021-12-06 23:26:46 -0800

    add unit tests

--- pipeline/pipeline_test.py
+++ pipeline/pipeline_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from .pipeline import Pipeline
 from .task import AbstractTask
@@ -7,8 +8,8 @@ from checkpoint_service import CheckpointKeySet, DisabledCheckpointKeySet
 TEST_CHECKPOINT_FILE = 'pipeline/test_data/pipeline_steps.log'
 
 class AppendTask(AbstractTask):
-    def __init__(self, name, number: int, result):
-        super().__init__(name)
+    def __init__(self, name, number: int, result, skip=False):
+        super().__init__(name, skip=skip)
         self.number = number
         self._result = result
 
@@ -46,11 +47,45 @@ class PipelineTest(unittest.TestCase):
         pipeline.run()
         self.assertEqual(result, [])
 
+    def test_run_with_skip_tasks(self):
+        result = []
+        test_pipeline_steps_key_set = DisabledCheckpointKeySet()
+        task_skip_boolean_tuples = [("task1", False),
+                                    ("task2", True),
+                                    ("task3", True),
+                                    ("task4", False)]
+        pipeline = self._create_test_pipeline_with_skipped_tasks(
+            test_pipeline_steps_key_set, task_skip_boolean_tuples, result)
+        pipeline.run()
+        self.assertEqual(result, [1, 4])
+
+    def test_run_with_skip_all_tasks(self):
+        result = []
+        test_pipeline_steps_key_set = DisabledCheckpointKeySet()
+        task_skip_boolean_tuples = [("task1", True),
+                                    ("task2", True),
+                                    ("task3", True),
+                                    ("task4", True)]
+        pipeline = self._create_test_pipeline_with_skipped_tasks(
+            test_pipeline_steps_key_set, task_skip_boolean_tuples, result)
+        pipeline.run()
+        self.assertEqual(result, [])
+
     def _create_test_pipeline(self, pipeline_steps_key_set, task_names, result):
         pipeline = Pipeline('test_data', pipeline_steps_key_set)
         parents = []
         for idx, task_name in enumerate(task_names):
             task = AppendTask(task_name, idx+1, result)
+            node = pipeline.add_task(task, parents)
+            parents = [node]
+
+        return pipeline
+
+    def _create_test_pipeline_with_skipped_tasks(self, pipeline_steps_key_set, task_name_skip_boolean_tuples, result):
+        pipeline = Pipeline('test_data', pipeline_steps_key_set)
+        parents = []
+        for idx, (task_name, skip) in enumerate(task_name_skip_boolean_tuples):
+            task = AppendTask(task_name, idx+1, result, skip=skip)
             node = pipeline.add_task(task, parents)
             parents = [node]
 
