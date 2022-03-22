eee6dbc9f3a698e9c491ac5344cd9d5acb03f115
Kevin Kim <kevinkim@databricks.com>
2022-03-21 19:16:40 -0700

    add test

--- dbclient/test/MLFlowClientTest.py
+++ dbclient/test/MLFlowClientTest.py
@@ -2,8 +2,11 @@ import unittest
 import os
 import sqlite3
 from dbclient import MLFlowClient
+from dbclient.test.TestUtils import TEST_CONFIG
+from checkpoint_service import CheckpointService, CheckpointKeyMap
 import json
 import concurrent.futures
+from unittest.mock import MagicMock
 from mlflow.entities import Metric, Param, RunTag, RunData, RunInfo, Run
 
 MLFLOW_TEST_FILE = "dbclient/test/mlflow_runs_test.db"
@@ -34,11 +37,26 @@ class MLFlowClientTest(unittest.TestCase):
         run = Run(
                     run_info=run_info,
                     run_data=RunData(
-                    metrics=metrics,
-                    params=params,
-                    tags=tags))
+                        metrics=metrics,
+                        params=params,
+                        tags=tags))
         runs_dict[run_id] = run
         return run
+
+    def _run_to_dict(self, run):
+        info = run.info
+        data = run.data
+        metrics = data.metrics
+        params = data.params
+        tags = data.tags
+        return {
+            "info": {
+                "experiment_id": info.experiment_id
+            },
+            "tags": tags,
+            "metrics": metrics,
+            "params": params
+        }
 
     def _insert_run_data(self, run):
         con = sqlite3.connect(MLFLOW_TEST_FILE, timeout=30)
@@ -80,7 +98,6 @@ class MLFlowClientTest(unittest.TestCase):
         assert(dict(run.data.metrics) == fetched_run_metrics)
         assert(dict(run.data.params) == fetched_run_params)
         assert(dict(run.data.tags) == fetched_run_tags)
-
 
     def test_save_run_data_to_sql_multiple_times(self):
         # Input data
@@ -170,3 +187,35 @@ class MLFlowClientTest(unittest.TestCase):
             assert(dict(runs_dict[id].data.metrics) == fetched_run_metrics)
             assert(dict(runs_dict[id].data.params) == fetched_run_params)
             assert(dict(runs_dict[id].data.tags) == fetched_run_tags)
+
+
+    def test_create_run_and_log_thread_safety(self):
+        checkpoint_service = MagicMock()
+        mlflow_client = MLFlowClient(TEST_CONFIG, checkpoint_service)
+
+        runs_dict = {}
+        unique_num_runs = 100
+        # Create duplicate run_id runs
+        runs = [self._generate_run(i, runs_dict) for i in range(1, 1 + unique_num_runs)]
+        runs += [self._generate_run(i, runs_dict) for i in range(1, 1 + unique_num_runs - 50)]
+        runs += [self._generate_run(i, runs_dict) for i in range(1, 1 + unique_num_runs)]
+        runs += [self._generate_run(i, runs_dict) for i in range(1, 1 + unique_num_runs - 25)]
+
+        # sort it by the run_id, so that threads have higher chance to access the same run_id during testing.
+        runs = sorted(runs, key=lambda run: run.info.run_id)
+        experiment_id_map = {
+            "experiment_id": "experiment_id_new"
+        }
+        error_logger = None
+        checkpointer = CheckpointKeyMap("dbclient/test/test_ml_run_import_checkpoint_temp.log")
+        mlflow_client._create_run_and_log_helper = MagicMock()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+            futures = [executor.submit(mlflow_client._create_run_and_log("", run.info.run_id, run.info.start_time, self._run_to_dict(run), experiment_id_map, error_logger, checkpointer)) for run in runs]
+            concurrent.futures.wait(futures)
+
+        assert(mlflow_client._create_run_and_log_helper.call_count == unique_num_runs)
+
+        os.remove("dbclient/test/test_ml_run_import_checkpoint_temp.log")
+
+
+    # TODO(kevin): Add more unit tests later
